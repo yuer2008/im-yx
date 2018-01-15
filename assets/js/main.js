4ab1ec49a3bc97867b9b5ac63c0f3cc0b89c7fe8
yuer2008 <yangfutao2000@163.com>
2018-01-15 10:02:23 +0800

    axios

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,4 @@ var now_time = function(){
 	var m = d.getMonth()+1;
 	return d.getFullYear() +'-'+ m +'-' + d.getDate() +' '+ d.getHours() +':'+ d.getMinutes() +':'+ d.getSeconds();
 };
-var check_status = function(ws){
-	if(typeof ws != 'undefined'){
-		ws.readyState == 3
-		//layer.msg('connection is closed');
-	}
-    
-};
+
